23fb316631b85bd34b2d070c40ae6dea1818d6d6
Rajdeep <rajdeep@atlogys.com>
2017-10-05 17:44:07 +0530

    Added story file for followup

---
+++ src/main/java/expertchat/bussinesslogic/FollowUp.java
@@ -0,0 +1,10 @@
+package expertchat.bussinesslogic;
+
+import expertchat.apioperation.AbstractApiFactory;
+import expertchat.apioperation.ExpertChatEndPoints;
+import expertchat.apioperation.apiresponse.HTTPCode;
+
+public class FollowUp extends AbstractApiFactory implements HTTPCode, ExpertChatEndPoints {
+
+
+}
---
+++ src/main/java/expertchat/test/bdd/FollowUpTC.java
@@ -0,0 +1,10 @@
+package expertchat.test.bdd;
+
+import com.relevantcodes.extentreports.ExtentReports;
+
+public class FollowUpTC extends AbstractSteps {
+    public FollowUpTC(ExtentReports reports, String casName) {      super(reports, casName);    }
+
+
+
+}
